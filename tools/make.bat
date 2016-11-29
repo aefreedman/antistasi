63db6a228dbb402bd35786f64c6116414687252c
Nate Berkopec <nate.berkopec@gmail.com>
2016-11-29 13:29:01 -0700

    Overwrite existing release copies

--- tools/make.bat
+++ tools/make.bat
@@ -1,2 +1,2 @@
 makepbo -JNUP . .\releases\antistasi-spacehunnies.Altis.pbo
-cp ".\releases\antistasi-spacehunnies.Altis.pbo" "C:\Program Files (x86)\Steam\steamapps\common\Arma 3\MPMissions\"
+copy /y ".\releases\antistasi-spacehunnies.Altis.pbo" "C:\Program Files (x86)\Steam\steamapps\common\Arma 3\MPMissions"
